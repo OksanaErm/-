78cddb45f533d18d4b7b37b4a9ff2d0045fa5995
Oksana <Rain-906@yandex.ru>
2020-04-20 16:30:19 +0300

    Добавьте файлы проекта.

---
+++ Калькулятор/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Калькулятор
+/*написать калькулятор*/
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double FirstValue, SecondValue;
+            string Action;
+
+            Console.WriteLine("Введите первое число");
+            FirstValue = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            SecondValue = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите действие '+' '-' '*' '/'");
+            Action = Console.ReadLine();
+
+            switch (Action)
+
+            {
+                default:
+                    Console.WriteLine("Error!");
+                    break;
+
+                case "+":
+                    Console.WriteLine(FirstValue + SecondValue);
+                    break;
+
+                case "-":
+                    Console.WriteLine(FirstValue - SecondValue);
+                    break;
+
+                case "*":
+                    Console.WriteLine(FirstValue * SecondValue);
+                    break;
+
+                case "/":
+                    Console.WriteLine(FirstValue / SecondValue);
+                    break;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
