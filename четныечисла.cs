8610fec997d76ecda80204f919d69681c1489b57
Oksana <Rain-906@yandex.ru>
2020-04-22 17:49:10 +0300

    add четные числа

---
+++ четныечисла.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace вывести_четные_числа
+{
+
+    class Program
+
+    {
+        public static double Main()
+
+        {
+            for (double i = 0; i <= 200; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.ReadKey();
+            return 0;
+        }
+    }
+
+}
